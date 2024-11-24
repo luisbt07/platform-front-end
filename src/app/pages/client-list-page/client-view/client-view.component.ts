47317c401f1f5069130b2bdb1deacd91dedc5524
luis.teixeira <luis.teixeira@loggi.com>
2024-11-24 17:21:55 -0300

    refact(): make project run

--- src/app/pages/client-list-page/client-view/client-view.component.ts
+++ src/app/pages/client-list-page/client-view/client-view.component.ts
@@ -1,14 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-view',
-  imports: [CommonModule],
-  templateUrl: './view.component.html',
-  styleUrl: './view.component.css'
+  selector: 'app-client-view',
+  imports: [],
+  templateUrl: './client-view.component.html',
+  styleUrl: './client-view.component.css'
 })
-
-export class ViewComponent {
+export class ClientViewComponent {
   clientes = [
     { nome: 'Kadin Herwitz', cnpj: '12.3425.678/9101-11', ciclo: 'Mensal', docs: 'CTE', criadoEm: '01/01/2023' },
     // Adicione mais objetos conforme necessário
