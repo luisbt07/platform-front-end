1e4d2a12eda2fa99500a1a9200a369f632f78771
luis.teixeira <luis.teixeira@loggi.com>
2024-12-06 10:52:12 -0300

    feat(): user json server to retrieve Company instances

--- src/app/pages/invoice-configuration-view/invoice-configuration.component.ts
+++ src/app/pages/invoice-configuration-view/invoice-configuration.component.ts
@@ -1,10 +1,11 @@
 import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SideBarMenuComponent } from "../../layout/side-bar-menu/side-bar-menu.component";
+import { CompanyService } from '../../services/company.service';
 import { InvoiceConfigurationService } from '../../services/invoice-configuration.service';
 import { InvoiceConfigurationMapper } from './invoice-configuration-class';
 import { InvoiceConfigData, InvoiceConfiguration } from './invoice-configuration.model';
-import { SideBarMenuComponent } from "../../layout/side-bar-menu/side-bar-menu.component";
 
 @Component({
   selector: 'app-invoice-configuration',
@@ -19,9 +20,13 @@ export class InvoiceConfigurationComponent {
   result!: string | InvoiceConfiguration[];
   showConfigs: boolean = false;
   inputData!: string;
+  companyId!: number | string
 
 
-  constructor(private invoiceConfigService: InvoiceConfigurationService) {
+  constructor(
+    private invoiceConfigService: InvoiceConfigurationService,
+    private companyService: CompanyService,
+  ) {
     this.sendTitle()
   }
 
@@ -30,16 +35,25 @@ export class InvoiceConfigurationComponent {
     this.getConfigurationsByInput(this.inputData)
   }
   getConfigurationsByInput(input: string) {
-    this.result = this.invoiceConfigService.getInvoiceConfigurations(input)
-    if (typeof this.result === 'string' || !this.result || this.result.length === 0) {
-      alert(this.result);
-    }
-    else {
-      for(const data of this.result) {
-        this.configs.push(new InvoiceConfigurationMapper(data).toObject())
-      };
-      this.showConfigs = true;
-    }
+    this.companyService.getCompanyId(input).subscribe((companyId) => {
+      this.companyId = companyId
+      if (typeof(this.companyId) === 'string') {
+        alert(this.companyId);
+      }
+      else {
+        this.result = this.invoiceConfigService.getInvoiceConfigurations(this.companyId)
+        if (typeof this.result === 'string' || !this.result || this.result.length === 0) {
+          alert(this.result);
+        }
+        else {
+          for(const data of this.result) {
+            this.configs.push(new InvoiceConfigurationMapper(data).toObject())
+          };
+          this.showConfigs = true;
+        }
+      }
+    })   
+    
   }
   sendTitle() {
     this.titleEvent.emit("Invoice Configuration View")
--- src/app/services/invoice-configuration.service.ts
+++ src/app/services/invoice-configuration.service.ts
@@ -1,31 +1,15 @@
 import { Injectable } from "@angular/core";
-import { InputValidator } from "../helpers/utils";
 import { INVOICE_CONFIGURATIONS } from "../pages/invoice-configuration-view/dummy-invoice-configs";
-import { InvoiceConfiguration } from "../pages/invoice-configuration-view/invoice-configuration.model";
-import { CompanyService } from "./company.service";
+import { type InvoiceConfiguration } from "../pages/invoice-configuration-view/invoice-configuration.model";
 
 @Injectable({providedIn: 'root'})
 export class InvoiceConfigurationService {
     private configurations = INVOICE_CONFIGURATIONS
 
-    constructor(private companyService: CompanyService) {}
-
-    getInvoiceConfigurations(input: string): InvoiceConfiguration[] | string {
+    getInvoiceConfigurations(companyId: number): InvoiceConfiguration[] | string {
         let filteredConfigs: InvoiceConfiguration[] = []
-        if (InputValidator.isValidCnpj(input)) {
-            let cleanedCnpj = input.replace(/\D/g, '');
-            const company = this.companyService.getCompanyByCnpj(cleanedCnpj)
-            if (company) {
-                filteredConfigs = this.retrieveInvoiceConfigsByCompanyId(company.id)
-            }
-            else {
-                return "No Company with this cnpj: " + cleanedCnpj
-            }
-        }
-        else if (InputValidator.isValidCompanyId(input)) {
-            let companyId: number = Number(input)
-            filteredConfigs = this.retrieveInvoiceConfigsByCompanyId(companyId)
-        }
+
+        filteredConfigs = this.retrieveInvoiceConfigsByCompanyId(companyId)
 
         return filteredConfigs.length > 0 ? filteredConfigs : "No configuration for this company"
     }
